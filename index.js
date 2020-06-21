a9df9e8f471dc2db860d24456f659784e945b293
Itacir Gabral <itacirgabral@gmail.com>
2020-06-21 16:07:56 -0400

    require duplicado

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const auth = require('basic-auth')
-const e = require('express')
 
 const app = express()
 app.use(cookieParser())
